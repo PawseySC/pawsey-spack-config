0e886647a9f7093ff078c963bb2a2bc160009007
Pascal Elahi <pascaljelahi@gmail.com>
2021-12-17 02:44:05 -0600

    Update to recipe to apply configure patch better testing mpi

--- setonix/repo_setonix/packages/exabayes/package.py
+++ setonix/repo_setonix/packages/exabayes/package.py
@@ -12,31 +12,34 @@ class Exabayes(AutotoolsPackage):
     url      = "https://cme.h-its.org/exelixis/resource/download/software/exabayes-1.5.1.tar.gz"
 
     version('1.5.1', sha256='f75ce8d5cee4d241cadacd0f5f5612d783b9e9babff2a99c7e0c3819a94bbca9')
-    variant('openmpi', default=True, description='Enable openMP parallel support')
 
-    #Spack spec fails if this line is not commented out. 
-    #depends_on('openmpi', when='+openmpi')
+    variant('mpi', default=True, description='Enable MPI parallel support')
 
-    variant('mpich', default=False, description='Enable mpich parallel support')
-    depends_on('cray-mpich', when='+mpich')
+    depends_on('mpi', when='+mpi')
 
     # ExaBayes manual states the program succesfully compiles with GCC, version
-    # 4.6 or greater, and Clang, version 3.2 or greater.
-    conflicts('%gcc@:4.5.4')
+    # 4.6 or greater, and Clang, version 3.2 or greater. The build fails when
+    # GCC 7.1.0 is used.
+    conflicts('%gcc@:4.5.4, 7.1.0:', when='@:1.5.0')
     conflicts('%clang@:3.1')
 
-    def configure_args(self):
-        args = []
-        if '+mpich' in self.spec:
-            args.append('--enable-mpich')
-        else:
-            args.append('--disable-mpich')
-        return args
+    # configure updated to better determine if MPI compiler available
+    patch('configure_mpi.patch', level=0, when='@1.5.1: +mpi', )
 
     def configure_args(self):
         args = []
-        if '+openmpi' in self.spec:
-            args.append('--enable-openmpi')
+        if '+mpi' in self.spec:
+            args.append('--enable-mpi')
         else:
-            args.append('--disable-openmpi')
+            args.append('--disable-mpi')
         return args
+
+    # don't do anything autoreconf as configure 
+    # in exabayes is properly setup and
+    # the autotools are not 
+    def autoreconf(self, spec, prefix):
+       return
+
+
+
+
