d6652196794b6574944b541404fce773d8dfc116
Marco De La Pierre <marco.delapierre@gmail.com>
2022-01-25 15:17:24 +0800

    setonix/extra/ : example install script on Joey for personal use

---
+++ setonix/extra/marco-setup-joey.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+  
+# minimal script to initialise Spack on Joey for personal testing
+# everything is done in the current working directory
+
+# this is the date tag to be replaced in the config yamls, with the current directory
+date_tag="2022.01"
+
+
+if [ -e ~/.spack ] ; then
+  mv ~/.spack ~/.spack.old
+fi
+mkdir ~/.spack
+
+git clone https://github.com/pawseysc/pawsey-spack-config
+git clone https://github.com/pawseysc/spack
+cd spack/
+git checkout v0.17.0
+cd ..
+
+cp -p pawsey-spack-config/setonix/configs_site_allusers/*.yaml spack/etc/spack/
+cp -p pawsey-spack-config/setonix/configs_spackuser_pawseystaff/*.yaml ~/.spack/
+cp -p pawsey-spack-config/setonix/fixes/microarchitectures.json spack/lib/spack/external/archspec/json/cpu/
+patch spack/lib/spack/spack/modules/lmod.py pawsey-spack-config/setonix/fixes/lmod_arch_family.patch
+
+# Joey specific
+rm -r spack/etc/spack/upstreams.yaml ~/.spack/upstreams.yaml
+sed -i '/USER/ s;^;#;g' spack/etc/spack/repos.yaml
+install_dir="$(pwd)"
+sed -i "s;/software/setonix/$date_tag;$install_dir;g" spack/etc/spack/*.yaml ~/.spack/*.yaml
