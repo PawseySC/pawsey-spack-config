06e5eaa867cd5fe9c041b8973ea1635a6fc5a0b7
Marco De La Pierre <marco.delapierre@gmail.com>
2022-02-02 17:35:47 +0800

    setonix: shpc setup and configuration

--- setonix/setup/setup_shpc.sh
+++ setonix/setup/setup_shpc.sh
@@ -21,9 +21,14 @@ pip install --prefix=$(pwd) singularity-hpc==$shpc_version
 # get registry from github repo
 git clone https://github.com/singularityhub/singularity-hpc
 cd singularity-hpc
+# checkout registry, too, for reproducibility
 git checkout $shpc_version
 cd ..
 mv singularity-hpc/registry .
+cd lib/python${python_version_major}.${python_version_minor}/site-packages
+# symlink here, so that registry is in a more visible location
+ln -s ../../../registry .
+cd ../../..
 rm -fr singularity-hpc
 
 # fix long shebang
@@ -36,7 +41,32 @@ export PATH=$(pwd)/bin:$PATH
 export PYTHONPATH=$(pwd)/lib/python${python_version_major}.${python_version_minor}/site-packages:$PYTHONPATH
 
 #### ALL SHPC CONFIG COMMANDS HERE
+# in alternative, we could provide edited yamls, just to copy over
+## ALL USERS
+# user install location for modulefiles
+shpc config set module_base:/software/\$PAWSEY_PROJECT/\$USER/setonix/containers/modules
+# user install location for containers
+shpc config set container_base:/software/\$PAWSEY_PROJECT/\$USER/setonix/containers/sif
+# custom Pawsey registry
+shpc config add registry:/software/setonix/2022.01/pawsey-spack-config/setonix/registry_setonix
+# singularity module
+shpc config set singularity_module:singularity/3.8.5
+# disable default version for modulefiles
+shpc config set default_version:false
+# enable wrapper scripts
+#shpc config set wrapper_scripts:true
+# GPU support (Phase 2)
+#shpc config set container_features:gpu:amd
+# enable X11 graphics
+shpc config set container_features:x11:true
+# location for container fake home
+shpc config set container_features:home:\$HOME/.shpc_home
 
-
+## SPACK USER
+shpc config inituser
+# system install location for modulefiles
+shpc config set module_base:/software/setonix/2022.01/containers/modules
+# system install location for containers
+shpc config set container_base:/software/setonix/2022.01/containers/sif
 
 cd ..
