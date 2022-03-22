0a7360c960d9f640a9a49eb024dad6611704f622
Marco De La Pierre <marco.delapierre@gmail.com>
2022-03-22 21:00:15 +0800

    setonix/setup: minor typo in module_spack

--- setonix/setup/module_spack.lua
+++ setonix/setup/module_spack.lua
@@ -7,7 +7,6 @@ whatis([[Version : SPACK_VERSION]])
 
 whatis([[Short description : A package management tool designed to support multiple versions and configurations of software on a wide variety of platforms and environments. ]])
 help([[A package management tool designed to support multiple versions and configurations of software on a wide variety of platforms and environments.]])
-usable
 
 load("PYTHON_MODULEFILE")
 
@@ -15,7 +14,7 @@ setenv("SPACK_HOME","/software/setonix/DATE_TAG/spack")
 
 -- Lmod 8.6+ has a function to source shell files in an unloadable way
 -- see https://lmod.readthedocs.io/en/latest/050_lua_modulefiles.html
--- Joey has 8.3, so this is not 
+-- Joey has 8.3, so this is not usable
 
 -- The following is NOT unloadable
 execute{cmd=". /software/setonix/DATE_TAG/spack/share/spack/setup-env.sh", modeA={"load"}}
