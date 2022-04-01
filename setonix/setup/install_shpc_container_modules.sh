1da9e8e122ccaa7de8b155643a6aea4d8043f9a7
Marco De La Pierre <marco.delapierre@gmail.com>
2022-04-01 13:37:58 +0800

    setonix/setup: updates to install shpc containers, including openfoam prefix

--- setonix/setup/install_shpc_container_modules.sh
+++ setonix/setup/install_shpc_container_modules.sh
@@ -51,11 +51,20 @@ done
 # create compact, symlinked module tree
 mkdir -p containers/${shpc_spackuser_modules_dir_short}
 cd containers/${shpc_spackuser_modules_dir_short}
-# populate symlinked module tree
+# avoid repetitions in symlinking
+container_tool_list=""
 for container in $container_list ; do
   container_tool=${container%:*}
+  container_tool_list+="$container_tool "
+done
+unique_container_tool_list="$(echo $container_tool_list | xargs -n 1 | sort | uniq | xargs)"
+# populate symlinked module tree
+for container_tool in $unique_container_tool_list ; do
   container_tool_short=${container_tool##*/}
   ln -s ${root_dir}/containers/${shpc_spackuser_modules_dir_long}/${container_tool} ${container_tool_short}
+  if [ "$container_tool_short" == "openfoam" ] || [ "$container_tool_short" == "openfoam-org" ] ; then
+    mv ${container_tool_short} ${shpc_spackuser_openfoam_add_prefix}${container_tool_short}
+  fi
 done
 # it's the symlinked module tree that needs to go in MODULEPATH:
 # `module use ${root_dir}/containers/${shpc_spackuser_modules_dir_short}`
--- setonix/setup/variables.sh
+++ setonix/setup/variables.sh
@@ -9,7 +9,7 @@ root_dir="/software/setonix/${date_tag}"
 spack_version="0.17.0" # the prefix "v" is added in setup_spack.sh
 singularity_version="3.8.6" # has to match the version in the Spack env yaml
 shpc_name="shpc" # decide this once and for all
-shpc_version="0.0.48"
+shpc_version="0.0.51"
 
 # python (and py tools) versions
 python_name="python"
@@ -28,6 +28,7 @@ pawsey_modules_dir="pawsey-modules"
 # shpc module directory for SPACK USER (system wide installation)
 shpc_spackuser_modules_dir_long="modules-long"
 shpc_spackuser_modules_dir_short="modules"
+shpc_spackuser_openfoam_add_prefix="containerised-"
 
 # python version info (no editing needed)
 python_version_major="$( echo $python_version | cut -d '.' -f 1 )"
