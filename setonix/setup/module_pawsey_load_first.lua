8c095686330b9563e4ff0bfc4dff90a9c885a40f
Marco De La Pierre <marco.delapierre@gmail.com>
2022-04-07 14:18:24 +0800

    setonix/setup, module_pawsey_load_first: added prefix sw_env_ to local variables, as this module will be merged with Pawsey settings with other scopes, too

--- setonix/setup/module_pawsey_load_first.lua
+++ setonix/setup/module_pawsey_load_first.lua
@@ -9,25 +9,25 @@ helps([[A set of variable definitions to handle software modules on Setonix, inc
 
 -- Service variables for this module
 -- 
-local date_tag = "current"
+local sw_env_date_tag = "current"
 -- 
 -- NOTE: all definitions below need to be kept in sync with the
 -- corresponding values found in `variables.sh` in this same directory
 -- 
 -- This is handy for testing, as it is the only one to tweak
-local root_dir = "/software/setonix/" .. date_tag -- "ROOT_DIR"
+local sw_env_root_dir = "/software/setonix/" .. sw_env_date_tag -- "ROOT_DIR"
 -- 
-local shpc_modules_dir_short = "modules" -- SHPC_SPACKUSER_MODULES_DIR_SHORT"
-local pawsey_modules_dir = "pawsey-modules" -- PAWSEY_MODULES_DIR"
+local sw_env_shpc_modules_dir_short = "modules" -- SHPC_SPACKUSER_MODULES_DIR_SHORT"
+local sw_env_pawsey_modules_dir = "pawsey-modules" -- PAWSEY_MODULES_DIR"
 -- 
 -- These need to be checked at every OS update
-local gcc_version  = "10.3.0" -- "GCC_VERSION"
-local cce_version  = "12.0.1" -- CCE_VERSION"
-local aocc_version = "3.0.0" -- AOCC_VERSION"
+local sw_env_gcc_version  = "10.3.0" -- "GCC_VERSION"
+local sw_env_cce_version  = "12.0.1" -- CCE_VERSION"
+local sw_env_aocc_version = "3.0.0" -- AOCC_VERSION"
 
 -- List of Spack module categories
 -- update when new categories are added
-local module_categories = {
+local sw_env_module_categories = {
   "astro-applications",
   "bio-applications",
   "applications",
@@ -41,33 +41,33 @@ local module_categories = {
 }
 -- Count how many categories
 num_categories = 0
-for _ in pairs(module_categories) do num_categories = num_categories + 1 end
+for _ in pairs(sw_env_module_categories) do num_categories = num_categories + 1 end
 
 -- Query CPU architecture
-local host_cpu = subprocess("lscpu | grep 'Model name'")
-if ( string.match(host_cpu, "7763") ~= nil ) then
+local sw_env_host_cpu = subprocess("lscpu | grep 'Model name'")
+if ( string.match(sw_env_host_cpu, "7763") ~= nil ) then
   arch = "zen3"
 else
   arch = "zen2"
 end
 
 -- Root directories for Spack modules
-local spack_root = root_dir .. "/modules/" .. arch
-local gcc_root  = spack_root .. "/gcc/" .. gcc_version
-local cce_root  = spack_root .. "/cce/" .. cce_version
-local aocc_root = spack_root .. "/aocc/" .. aocc_version
+local sw_env_spack_root = sw_env_root_dir .. "/modules/" .. arch
+local sw_env_gcc_root  = sw_env_spack_root .. "/gcc/" .. sw_env_gcc_version
+local sw_env_cce_root  = sw_env_spack_root .. "/cce/" .. sw_env_cce_version
+local sw_env_aocc_root = sw_env_spack_root .. "/aocc/" .. sw_env_aocc_version
 
 -- Add Spack modules to Cray Lmod hierarchy variables
 for index = 1,num_categories do
-  prepend_path("LMOD_CUSTOM_COMPILER_GNU_PREFIX", gcc_root .. "/" .. module_categories[index])
-  prepend_path("LMOD_CUSTOM_COMPILER_CRAYCLANG_10_0_PREFIX", cce_root .. "/" .. module_categories[index])
-  prepend_path("LMOD_CUSTOM_COMPILER_AOCC_3_0_PREFIX", aocc_root .. "/" .. module_categories[index])
+  prepend_path("LMOD_CUSTOM_COMPILER_GNU_PREFIX", sw_env_gcc_root .. "/" .. sw_env_module_categories[index])
+  prepend_path("LMOD_CUSTOM_COMPILER_CRAYCLANG_10_0_PREFIX", sw_env_cce_root .. "/" .. sw_env_module_categories[index])
+  prepend_path("LMOD_CUSTOM_COMPILER_AOCC_3_0_PREFIX", sw_env_aocc_root .. "/" .. sw_env_module_categories[index])
 end
 
 -- Add SHPC modules to MODULEPATH
-local shpc_root = root_dir .. "/containers/" .. shpc_modules_dir_short
-prepend_path("MODULEPATH", shpc_root)
+local sw_env_shpc_root = sw_env_root_dir .. "/containers/" .. sw_env_shpc_modules_dir_short
+prepend_path("MODULEPATH", sw_env_shpc_root)
 
 -- Add Spack/SHPC modules to MODULEPATH
-local pawsey_modules_root = root_dir .. "/" .. pawsey_modules_dir
-prepend_path("MODULEPATH", pawsey_modules_root)
+local sw_env_pawsey_modules_root = sw_env_root_dir .. "/" .. sw_env_pawsey_modules_dir
+prepend_path("MODULEPATH", sw_env_pawsey_modules_root)
