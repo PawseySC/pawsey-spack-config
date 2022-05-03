164455e800e3ffae155a069c61100572b7091613
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2022-05-03 05:08:38 +0000

    Update to wsclean and adding wcstools

--- setonix/repo/packages/wsclean/package.py
+++ setonix/repo/packages/wsclean/package.py
@@ -17,9 +17,10 @@ class Wsclean(CMakePackage):
     """
 
     homepage = "https://wsclean.readthedocs.io/en/latest/"
-    url      = "https://sourceforge.net/projects/wsclean/files/wsclean-2.10/wsclean-2.10.1.tar.bz2/download"
+    url      = "https://sourceforge.net/projects/wsclean/files/wsclean-2.10/wsclean-2.10.1.tar.bz2"
 
-    version('2.10.1', sha256='d5dbd32b7a7f79baace09dd6518121798d2fcbb84b81046b61ff90f980c8f963')
+    version('2.10.1', sha256='778edc1e73ce346a62063eef570054c268727a0fab47b999549d678a8b26ee1e')
+    version('2.9', sha256='d3e6d2de3cb923f5fa37638977c58bbe56a9db79ac2523ef0d0dbb3c1afe065d')
 
     depends_on('chgcentre', type='build')
     depends_on('casacore')
@@ -28,61 +29,7 @@ class Wsclean(CMakePackage):
     depends_on('gsl')
     depends_on('cfitsio')
 
-
-    # maali cygnet has odd patching
-    #sed -i 's/fftw_make_planner_thread_safe()/void fftw_make_planner_thread_safe(void)/g' wsclean/*.cpp
-    #sed -i 's/fftwf_make_planner_thread_safe()/void fftwf_make_planner_thread_safe(void)/g' wsclean/*.cpp
-
     def url_for_version(self, version):
-        return ("https://sourceforge.net/projects/wsclean/files/wsclean-{0}/wsclean-{0}.tar.bz2/download".format(version))
+        version_numbers = str(version).split('.')
+        return ("https://sourceforge.net/projects/wsclean/files/wsclean-{0}/wsclean-{1}.tar.bz2".format(version_numbers[0]+'.'+version_numbers[1], version))
 
-#     def cmake_args(self):
-#         args = []
-#         spec = self.spec
-
-
-
-#     export CC=gcc
-#     export CXX="g++ -std=c++11" 
-# export CXXFLAGS="-fPIC -std=c++11"
-#     mkdir -p build
-#     cd build
-
-# maali_run "cmake ..  -DCMAKE_INSTALL_PREFIX=${MAALI_INSTALL_DIR} -DCFITSIO_ROOT_DIR=${MAALI_CFITSIO_HOME} -DIDGAPI_LIBRARIES=${MAALI_IDG_HOME}/lib/libidg-api.so -DIDGAPI_INCLUDE_DIRS=${MAALI_IDG_HOME}/include -DFFTW3_LIB=${MKLROOT}/lib/intel64 -DCASACORE_ROOT_DIR=${MAALI_CASACORE_HOME} -DFFTW3F_THREADS_LIB=${MKLROOT}/lib/intel64 -DGSL_INCLUDE_DIR=$MAALI_GSL_HOME/include -DGSL_LIB=$MAALI_GSL_HOME/lib/libgsl.a -DGSL_CBLAS_LIB=$MAALI_GSL_HOME/lib/libgslcblas.a -DFFTW3F_LIB=${MKLROOT}/lib/intel64 -DFFTW3_THREADS_LIB=${MKLROOT}/lib/intel64 -DFFTW3_INCLUDE_DIR=${MKLROOT}/include/fftw"
-
-#     maali_run "make -j12 VERBOSE=1"
-#     maali_run "make install"
-
-# }
-
-#         args.append(self.define_from_variant('ENABLE_SHARED', 'shared'))
-#         args.append(self.define_from_variant('USE_OPENMP', 'openmp'))
-#         args.append(self.define_from_variant('USE_READLINE', 'readline'))
-#         args.append(self.define_from_variant('USE_HDF5', 'hdf5'))
-#         args.append(self.define_from_variant('USE_ADIOS2', 'adios2'))
-#         args.append(self.define_from_variant('USE_MPI', 'adios2'))
-#         if spec.satisfies('+adios2'):
-#             args.append(self.define('ENABLE_TABLELOCKING', False))
-
-#         # fftw3 is required by casacore starting with v3.4.0, but the
-#         # old fftpack is still available. For v3.4.0 and later, we
-#         # always require FFTW3 dependency with the optional addition
-#         # of FFTPack. In older casacore versions, only one of FFTW3 or
-#         # FFTPack can be selected.
-#         if spec.satisfies('@3.4.0:'):
-#             if spec.satisfies('+fftpack'):
-#                 args.append('-DBUILD_FFTPACK_DEPRECATED=YES')
-#             args.append(self.define('USE_FFTW3', True))
-#         else:
-#             args.append(self.define('USE_FFTW3', spec.satisfies('~fftpack')))
-
-#         # Python2 and Python3 binding
-#         if spec.satisfies('~python'):
-#             args.extend(['-DBUILD_PYTHON=NO', '-DBUILD_PYTHON3=NO'])
-#         elif spec.satisfies('^python@3.0.0:'):
-#             args.extend(['-DBUILD_PYTHON=NO', '-DBUILD_PYTHON3=YES'])
-#         else:
-#             args.extend(['-DBUILD_PYTHON=YES', '-DBUILD_PYTHON3=NO'])
-
-#         args.append('-DBUILD_TESTING=OFF')
-#         return args
