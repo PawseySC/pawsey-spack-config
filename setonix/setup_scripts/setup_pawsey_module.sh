88a7446f278039f45f94da315c7e0b378ca40406
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-24 21:57:25 +0800

    setonix setup: fix to setup pawsey module

--- setonix/setup_scripts/setup_pawsey_module.sh
+++ setonix/setup_scripts/setup_pawsey_module.sh
@@ -14,12 +14,12 @@ script_dir="$(dirname $0 2>/dev/null || pwd)"
 cd ${root_dir}
 
 sed \
-  -e "s/TOP_ROOT_DIR/${top_root_dir}/g" \
-  -e "s/CUSTOM_MODULES_DIR/${custom_modules_dir}/g" \
-  -e "s/UTILITIES_MODULES_DIR/${utilities_modules_dir}/g" \
-  -e "s/SHPC_CONTAINERS_MODULES_DIR/${shpc_containers_modules_dir}/g" \
-  -e "s/GCC_VERSION/${gcc_version}/g" \
-  -e "s/CCE_VERSION/${cce_version}/g" \
-  -e "s/AOCC_VERSION/${aocc_version}/g" \
+  -e "s;TOP_ROOT_DIR;${top_root_dir};g" \
+  -e "s;CUSTOM_MODULES_DIR;${custom_modules_dir};g" \
+  -e "s;UTILITIES_MODULES_DIR;${utilities_modules_dir};g" \
+  -e "s;SHPC_CONTAINERS_MODULES_DIR;${shpc_containers_modules_dir};g" \
+  -e "s;GCC_VERSION;${gcc_version};g" \
+  -e "s;CCE_VERSION;${cce_version};g" \
+  -e "s;AOCC_VERSION;${aocc_version};g" \
   pawsey-spack-config/setonix/setup_scripts/module_pawsey_load_first.lua \
   > ${root_dir}/pawsey_load_first.lua
