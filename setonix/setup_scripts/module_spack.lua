1d07c919f2315c5fcf063c064838dee3268e4ea5
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-24 21:20:19 +0800

    setonix setup: added unload warning to spack module

--- setonix/setup_scripts/module_spack.lua
+++ setonix/setup_scripts/module_spack.lua
@@ -18,6 +18,10 @@ setenv("SPACK_HOME","/software/setonix/DATE_TAG/spack")
 
 -- The following is NOT unloadable
 execute{cmd=". /software/setonix/DATE_TAG/spack/share/spack/setup-env.sh", modeA={"load"}}
+-- Warn users about this fact
+if (mode() == "load" or mode() == "unload") then
+  LmodMessage("Note: when this module is unloaded, the shell environment will NOT revert to its original state, and retain some Spack settings. If you need the original shell environment, start a new shell session instead.")
+end
 
 -- Enforce explicit usage of versions by requiring full module name
 if (mode() == "load") then
