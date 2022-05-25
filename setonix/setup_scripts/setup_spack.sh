ddb757ec56403aa482776490ba02b5fba4cc1fa9
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-25 22:24:26 +0800

    setonix setup: pawsey module setup in setup_spack

--- setonix/setup_scripts/setup_spack.sh
+++ setonix/setup_scripts/setup_spack.sh
@@ -46,3 +46,18 @@ sed \
   -e "s/DATE_TAG/${date_tag}/g" \
   pawsey-spack-config/setonix/setup_scripts/module_spack.lua \
   > ${root_dir}/${spack_module_dir}/${spack_version}.lua
+
+# create a template for the pawsey module, inside ${root_dir},
+# containing the software stack related snippet,
+# to be handed over to the Platforms team
+cd ${root_dir}
+sed \
+  -e "s;TOP_ROOT_DIR;${top_root_dir};g" \
+  -e "s;CUSTOM_MODULES_DIR;${custom_modules_dir};g" \
+  -e "s;UTILITIES_MODULES_DIR;${utilities_modules_dir};g" \
+  -e "s;SHPC_CONTAINERS_MODULES_DIR;${shpc_containers_modules_dir};g" \
+  -e "s;GCC_VERSION;${gcc_version};g" \
+  -e "s;CCE_VERSION;${cce_version};g" \
+  -e "s;AOCC_VERSION;${aocc_version};g" \
+  pawsey-spack-config/setonix/setup_scripts/module_pawsey_load_first.lua \
+  > ${root_dir}/pawsey_load_first.lua
