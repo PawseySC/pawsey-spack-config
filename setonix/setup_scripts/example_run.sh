4cc3df58ed57b7eb87784acde7c21fe4694b9f7b
Marco De La Pierre <marco.delapierre@gmail.com>
2022-05-27 17:24:53 +0800

    setonix fix example run

--- setonix/setup_scripts/example_run.sh
+++ setonix/setup_scripts/example_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # for provisional setup (no spack modulepaths yet)
-is_avail_spack="$( module is-avail spack/${spack_version} ; echo "$?" )"
+is_avail_spack="$( module is-avail spack/0.17.0} ; echo "$?" )"
 if [ "${is_avail_spack}" != "0" ] ; then
-  module use ${root_dir}/${pawsey_temp}
-  module load ${pawsey_temp}
+  module use /software/setonix/2022.05/pawsey_temp
+  module load pawsey_temp
+  module swap PrgEnv-cray PrgEnv-gnu
 fi
 
 module load spack/0.17.0
