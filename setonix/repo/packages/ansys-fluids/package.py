4a1fb951e0e14413aff9eb4a5e0be0615a74c323
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2022-06-02 13:46:33 +0800

    Update to ansys fluids installation, issues if path is too long

--- setonix/repo/packages/ansys-fluids/package.py
+++ setonix/repo/packages/ansys-fluids/package.py
@@ -3,25 +3,46 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install ansys-fluids
+#
+# You can edit this file again by typing:
+#
+#     spack edit ansys-fluids
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
 import os
+import shutil
+
 import platform
 import re
 
 from spack.util.prefix import Prefix
+from spack import *
 
-class Ansysfluids(Package):
+class AnsysFluids(Package):
     """
     Ansys computational fluid dynamics (CFD) products are for engineers who need to make better, faster decisions. Our CFD simulation products have been validated and are highly regarded for their superior computing power and accurate results. Reduce development time and efforts while improving your product’s performance and safety.
     """
 
     homepage = "https://www.ansys.com/products/fluids"
-    url = "file://{0}/FLUIDS_{1}_LINUX64.tgz".format(os.getcwd(), version)
+    url = "file://{0}/FLUIDS2022R1.tgz".format(os.getcwd())
     manual_download = True
+    
 
     maintainers = ['Basha', 'Pascal Elahi']
+    
 
-    version('2022R1', sha256='??')
-    versiontoansysversion = ('2022R1': 'v221')
+    version('2022R1',sha256='cb61c6e48ad1272a9cf9d70afc228c0e5c0deef0ba8264a9b40fbef6ceedf351')
+    versiontoansysversion = ('2022R1', 'v221')
 
     preferred_version = "2022R1"
 
@@ -29,35 +50,29 @@ class Ansysfluids(Package):
     def install(self, spec, prefix):
         # maali code has 
         # make ./INSTALL in src directory executable and then run 
-        # idea is should have something like the following
-        # based on amber
-        install_tree('ansysfluids_tmpdir', '.')
-        shutil.rmtree(join_path(self.stage.source_path, 'ansysfuilds_tmpdir'))
-        # run the executable that does it all 
-        run_install = Executable("./INSTALL -silent -install_dir {0} -usetempdir {1}".format(self.prefix???, self.stage.source_path))
-        # need to look into how spack wants to define the install_directory and build_directory
-
+	# run the executable that does it all 
+        mkdirp('{0}/{1}'.format(self.stage.source_path, 'ansysfluids_tmpdir'))
+        run_install = Executable("./INSTALL -silent -install_dir {0} -usetempdir {1}/{2}".format(self.prefix, self.stage.source_path, 'ansysfluids_tmpdir'))
+        run_install()
         # do not set group permission for ansys here, do it in a follow up script
 
     def setup_run_environment(self, env):
         #env.set('ANSYS_VERSION', versiontoansysversion[self.version])
-        env.prepend_path('PATH', "{0}/{1}/Framework/bin/Linux64/".format(self.prefix, versiontoansysversion[self.version]))
-        env.prepend_path('PATH', "{0}/{1}/ansys/bin/".format(self.prefix, versiontoansysversion[self.version]))
+        env.prepend_path('PATH', "{0}/v221/Framework/bin/Linux64/".format(self.prefix))
+        env.prepend_path('PATH', "{0}/v221/fluent/bin/".format(self.prefix))
         ldpathlist=[
-            "/ansys/lib/linx64/", 
-            "/commonfiles/AAS/bin/linx64/", 
-            "/ansys/syslib/ansGRPC/",
-            "/commonfiles/Tcl/lib/linx64/", 
-            "/tp/zlib/1_2_11/linx64/lib/",
-            "/Electronics/Linux64/defer/"
-            "/tp/hdf5/1_10_5/linx64/lib 
+            "/fluent/lib/lnamd64/",
+            "/tp/hdf5/1_10_5/linx64/lib/",
+            "/fluent/fluent22.1.0/cortex/lnamd64/",
+            "/fluent/fluent22.1.0/multiport/lnamd64/mpi/shared/",
+            "/fluent/fluent22.1.0/multiport/mpi_wrapper/lnamd64/intel/",
+            "/tp/IntelMKL/2021.3.0/linx64/lib/intel64/",
             "/dcs/",
-            "/Framework/bin/Linux64/",
-            "/ansys/syslib/boost/",
-            "/ansys/lib/linx64/intel/",
-            "/ansys/lib/linx64/amd/",
-            "/commonfiles/AMD/BLIS/v3.0.0/linx64/lib/",
+            "/tp/IntelCompiler/2019.3.199/linx64/lib/intel64/",
+            "/fluent/fluent22.1.0/utility/viewfac/multiport/mpi/lnamd64/intel/lib/",
         ]
         for l in ldpathlist:
-            env.prepend_path('LD_LIBRARY_PATH', "{0}/{1}/{2}".format(self.prefix, versiontoansysversion[self.version],l))
+            env.prepend_path('LD_LIBRARY_PATH', "{0}/v221/{1}".format(self.prefix,l))
+
+
 
