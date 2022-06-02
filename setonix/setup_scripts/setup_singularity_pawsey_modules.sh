4a9d94f733ef0428ec149714508540b038e66c53
Marco De La Pierre <marco.delapierre@gmail.com>
2022-06-02 14:03:57 +0800

    setonix setup: update to singularity module script

--- setonix/setup_scripts/setup_singularity_pawsey_modules.sh
+++ setonix/setup_scripts/setup_singularity_pawsey_modules.sh
@@ -5,19 +5,35 @@
 script_dir="$(readlink -f "$(dirname $0 2>/dev/null)" || pwd)"
 . ${script_dir}/variables.sh
 
-# This script just does one thing
-# It creates a symlink from {spack modules}/{arch}/{compiler}
-# to the Pawsey utilities module directory
-# In this way, Singularity is available regardless the loaded PrgEng/compiler
-# It will still work, thanks to RPATH
+# This script creates end user singularity modules, starting from the Spack one
+# For each version:
+# 1. creates 2 pawsey/modules : A. singularity, and B. singularity-astro
+#    - to have Singularity available regardless the loaded PrgEng/compiler
+#      (works thanks to RPATHs)
+#    - to have /astro and /askapbuffer only for astronomers
+# 2. hides Spack module in {spack modules}/{arch}/{compiler}
+#    - to avoid confusion
 
+# define source (Spack) and destination (Pawsey) directories for Singularity
 src_dir="${root_dir}/modules/${cpu_arch}/gcc/${gcc_version}/utilities/${singularity_name}"
 dst_dir="${root_dir}/${singularity_symlink_module_dir}"
 
+# ensure destination directory exists
 mkdir -p ${dst_dir}
 
 for version in $( ls ${src_dir} ) ; do
-  ln -s ${src_dir}/${version} ${dst_dir}/${version}
+  # 1.B singularity-astro is just the original module
+  cp -p ${src_dir}/${version} ${dst_dir}/${version/\.lua/-astro.lua}
+  # 1.A singularity does not bind mount /askapbuffer and /astro
+  sed \
+    -e '/SINGULARITY_BINDPATH/ s;/askapbuffer;;g' \
+    -e '/SINGULARITY_BINDPATH/ s;/astro;;g' \
+    ${dst_dir}/${version/\.lua/-astro.lua} \
+    >${dst_dir}/${version}
+  #2. hide Spack module
+  rm -f ${src_dir}/.modulerc.lua
+  echo "hide_modulefile(\"${src_dir}/${version}\")" >>${src_dir}/.modulerc.lua
+  echo "hide_modulefile(\"${src_dir/${date_tag}/current}/${version}\")" >>${src_dir}/.modulerc.lua
 done
 
 
