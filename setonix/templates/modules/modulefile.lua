c81bdfb7da12b3e1ccb89e4e6930eba804ad5830
Marco De La Pierre <marco.delapierre@gmail.com>
2022-06-02 23:01:45 +0800

    setonix singularity: update paths in template lua, for mpi

--- setonix/templates/modules/modulefile.lua
+++ setonix/templates/modules/modulefile.lua
@@ -97,8 +97,8 @@ unsetenv("{{ cmd.name }}")
 -- MPI + Singularity configuration
 -- TODO: review on Setonix Phase 1
 -- TODO: do we need something for /var/spool/slurmd ?
-setenv("SINGULARITYENV_LD_LIBRARY_PATH","/opt/cray/pe/mpich/default/ofi/gnu/9.1/lib-abi-mpich:/opt/cray/xpmem/default/lib64:/opt/cray/pe/pmi/default/lib:/opt/cray/libfabric/1.11.0.3.66/lib64:/opt/cray/pe/gcc-libs:/usr/lib64:/usr/lib64/libibverbs:$LD_LIBRARY_PATH")
-setenv("SINGULARITY_BINDPATH","/askapbuffer,/astro,/scratch,/software,/var/opt/cray/pe,/etc/opt/cray/pe,/opt/cray,/etc/alternatives/cray-dvs,/etc/alternatives/cray-xpmem,/opt/cray/pe/lib64/libpmi2.so.0,/opt/cray/pe/lib64/libpmi.so.0,/usr/lib64/librdmacm.so.1,/usr/lib64/libibverbs.so.1,/usr/lib64/libpals.so.0,/usr/lib64/libnl-3.so.200,/usr/lib64/libnl-route-3.so.200,/etc/libibverbs.d,/usr/lib64/libibverbs,/usr/lib64/libmlx5.so.1.14.30.0")
+setenv("SINGULARITYENV_LD_LIBRARY_PATH","/opt/cray/pe/mpich/default/ofi/gnu/9.1/lib-abi-mpich:/opt/cray/pe/mpich/default/gtl/lib:/opt/cray/xpmem/default/lib64:/opt/cray/pe/pmi/default/lib:/opt/cray/pe/pals/default/lib:/opt/cray/libfabric/1.11.0.4.75/lib64:/opt/cray/pe/gcc-libs:/usr/lib64:/usr/lib64/libibverbs:$LD_LIBRARY_PATH")
+setenv("SINGULARITY_BINDPATH","/askapbuffer,/astro,/scratch,/software,/var/opt/cray/pe,/etc/opt/cray/pe,/opt/cray,/etc/alternatives/cray-dvs,/etc/alternatives/cray-xpmem,/usr/lib64/librdmacm.so.1,/usr/lib64/libibverbs.so.1,/usr/lib64/libpals.so.0,/usr/lib64/libnl-3.so.200,/usr/lib64/libnl-route-3.so.200,/usr/lib64/libmlx5.so.1.14.30.0,/etc/libibverbs.d,/usr/lib64/libibverbs,/var/spool")
 -- LD_PRELOAD edited to fix missing library dependency of Cray libmpi on libxpmem
 prepend_path("SINGULARITYENV_LD_PRELOAD","/opt/cray/xpmem/default/lib64/libxpmem.so.0")
 -- TODO: check if the 2 below are still needed (taken from Magnus)
