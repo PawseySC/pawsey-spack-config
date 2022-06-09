658f3491920b71e03279a109aeffa45f0b136477
Marco De La Pierre <marco.delapierre@gmail.com>
2022-06-09 12:47:29 +0800

    setonix personal setup: minor edits

--- setonix/personal_setup/personal_setup_spack.sh
+++ setonix/personal_setup/personal_setup_spack.sh
@@ -21,22 +21,29 @@ mkdir ~/.spack
 # get spack and its config from github
 git clone https://github.com/pawseysc/pawsey-spack-config
 git clone https://github.com/pawseysc/spack
-cd spack/
+cd spack
 git checkout v${spack_version}
 cd ..
 
 # copy configs into spack tree
 cp -p pawsey-spack-config/setonix/personal_setup/*.yaml spack/etc/spack/
 
-# edit DATE_TAG in sourceable script with spack functions
-sed -i "s;date_tag=.*;date_tag=${date_tag} # DATE_TAG;g" pawsey-spack-config/setonix/personal_setup/source_pawsey_spack_cmds.sh
+# set dummy DATE_TAG in sourceable script with spack functions
+sed -i "s;date_tag=.*;date_tag=personal # DATE_TAG;g" \
+  pawsey-spack-config/setonix/personal_setup/source_pawsey_spack_cmds.sh
 
 # apply fixes into spack tree
 # Marco,s Lmod arch family fix for the module tree
-patch spack/lib/spack/spack/modules/lmod.py pawsey-spack-config/setonix/fixes/lmod_arch_family.patch
+patch \
+  spack/lib/spack/spack/modules/lmod.py \
+  pawsey-spack-config/setonix/fixes/lmod_arch_family.patch
 # Pascal,s enhancements to modulefiles
-patch spack/lib/spack/spack/modules/common.py pawsey-spack-config/setonix/fixes/modulenames_plus_common.patch
-patch spack/lib/spack/spack/cmd/modules/__init__.py pawsey-spack-config/setonix/fixes/modulenames_plus_init.patch
+patch \
+  spack/lib/spack/spack/modules/common.py \
+  pawsey-spack-config/setonix/fixes/modulenames_plus_common.patch
+patch \
+  spack/lib/spack/spack/cmd/modules/__init__.py \
+  pawsey-spack-config/setonix/fixes/modulenames_plus_init.patch
 
 
 # protecting from accidental installations
