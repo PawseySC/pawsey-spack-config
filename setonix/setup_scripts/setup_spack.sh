0ddfd130124018419c6e3c9c33cab55348ac097b
Marco De La Pierre <marco.delapierre@gmail.com>
2022-06-10 11:15:18 +0800

    setonix setup: spack script now needs date_tag as argument

--- setonix/setup_scripts/setup_spack.sh
+++ setonix/setup_scripts/setup_spack.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# need to provide DATE_TAG as arg
+if [ "$#" -eq 0 ] ; then
+  echo "An argument is required, providing the installation DATE_TAG."
+  echo "The typical format is \"YYYY.MM\", e.g. \"2022.01\"."
+  echo "Exiting now."
+  exit 1
+else
+  new_date_tag="$1"
+fi
+
+
 # protecting from accidental installations
 echo "Do you want to install Spack on this system? (yes/no)"
 read install_answer
@@ -8,6 +19,27 @@ if [ ${install_answer,,} != "yes" ] ; then
   exit
 else
 
+
+# test that repo location and provided DATE_TAG are consistent with installation
+present_script_dir="$(readlink -f "$(dirname $0 2>/dev/null)" || readlink -f "$(pwd)")"
+present_root_dir="${present_script_dir%/pawsey-spack-config*}"
+eval $( echo $( grep 'top_root_dir=' ${present_script_dir}/variables.sh ) )
+new_root_dir="${top_root_dir}/${new_date_tag}"
+if [ "${present_root_dir}" != "${new_root_dir}" ] ; then
+  echo "You are willing to deploy this installation in: ${new_root_dir}"
+  echo "However, you are running from: ${present_root_dir}"
+  echo "Please make sure to clone this repo in the right location, e.g.:"
+  echo "  mkdir -p ${new_root_dir}"
+  echo "  cd ${new_root_dir}"
+  echo "  git clone https://github.com/pawseysc/pawsey-spack-config"
+  echo "Exiting."
+  exit 1
+else
+  # updating date_tag in variables.sh
+  sed -i "s;date_tag=.*;date_tag=\"${new_date_tag}\";g" \
+    ${present_script_dir}/variables.sh
+  echo "Deploying the installation in: ${present_root_dir}."
+fi
 
 # source setup variables
 # if copy/pasting these commands, need to run from this directory
@@ -21,8 +53,9 @@ fi
 mkdir ~/.spack
 
 # get spack and its config from github
-git clone https://github.com/pawseysc/pawsey-spack-config \
-  ${root_dir}/pawsey-spack-config
+# now assuming that pawsey-spack-config is cloned beforehand
+#git clone https://github.com/pawseysc/pawsey-spack-config \
+#  ${root_dir}/pawsey-spack-config
 git clone https://github.com/pawseysc/spack \
   ${root_dir}/spack
 cd ${root_dir}/spack
@@ -41,7 +74,7 @@ sed -i "s/DATE_TAG/$date_tag/g" \
   ${root_dir}/spack/etc/spack/*.yaml ~/.spack/*.yaml
 
 # edit DATE_TAG in sourceable script with spack functions
-sed -i "s;date_tag=.*;date_tag=${date_tag} # DATE_TAG;g" \
+sed -i "s;date_tag=.*;date_tag=\"${date_tag}\" # DATE_TAG;g" \
   ${script_dir}/source_pawsey_spack_cmds.sh
 
 # apply fixes into spack tree
