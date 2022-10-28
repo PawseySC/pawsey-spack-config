41210757af3a467269a368c7d35f13ade9a1ff1e
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2022-10-28 16:13:56 +0800

    Updates

--- setonix/setup_scripts/install_software_stack.sh
+++ setonix/setup_scripts/install_software_stack.sh
@@ -8,3 +8,9 @@ export top_root_dir="${MYSOFTWARE}/setonixtrial"
 
 ./run_first_python_install.sh
 
+module unload pawsey_prgenv
+module use ${top_root_dir}/${date_tag}/pawsey_temp
+module load pawsey_temp
+module load spack/0.17.0
+
+./run_concretization.sh
