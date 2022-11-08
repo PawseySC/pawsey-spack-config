b50da332e35d5a2d619765375d64f86026d59f31
Cristian Di Pietrantonio <cdipietrantonio@pawsey.org.au>
2022-11-08 10:28:31 +0800

    Improvements to the installer script.

--- setonix/setup_scripts/install_software_stack.sh
+++ setonix/setup_scripts/install_software_stack.sh
@@ -27,11 +27,14 @@ module use ${top_root_dir}/${date_tag}/pawsey_temp
 # we need the python module to be available in order to run spack
 module use ${top_root_dir}/${date_tag}/modules/zen3/gcc/12.1.0/programming-languages
 module --ignore-cache load pawsey_temp
+# swap is needed for the pawsey_temp module to work
+module swap PrgEnv-gnu PrgEnv-cray
+module swap PrgEnv-cray PrgEnv-gnu
 module load spack/0.17.0
 
-spack -d install singularity 
 echo "Run concretization.."
 ./run_concretization.sh
 
 echo "Run install all.."
 ./run_installation_all.sh
+
