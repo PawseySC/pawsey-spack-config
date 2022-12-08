c2cc4c51458c5a67964ab795821e68bb604ca59f
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2022-12-08 21:52:04 +0800

    Reverts commit 4ecb7697d. We do not want to use cray-python.

--- scripts/run_installation_all.sh
+++ scripts/run_installation_all.sh
@@ -13,7 +13,13 @@ fi
 PAWSEY_SPACK_CONFIG_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 . "${PAWSEY_SPACK_CONFIG_REPO}/systems/${SYSTEM}/settings.sh"
 
-. "${INSTALL_PREFIX}/spack/share/spack/setup-env.sh"
+module use ${INSTALL_PREFIX}/staff_modulefiles
+# we need the python module to be available in order to run spack
+module --ignore-cache load pawseyenv/${pawseyenv_version}
+# swap is needed for the pawsey_temp module to work
+module swap PrgEnv-gnu PrgEnv-cray
+module swap PrgEnv-cray PrgEnv-gnu
+module load spack/${spack_version}
 
 # list of environments included in variables.sh (sourced above)
 envdir="${PAWSEY_SPACK_CONFIG_REPO}/systems/${SYSTEM}/environments"
