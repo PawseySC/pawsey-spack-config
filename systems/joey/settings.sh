950955378438c61a50114d9120725acf88db85ab
Pascal Elahi <pascaljelahi@gmail.com>
2023-02-03 10:56:16 +0800

    Added Joey configuration
    - Added configuration for Joey
    - Configuration is set up for Casinni NICs and SS>=11

---
+++ systems/joey/settings.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+if [ -z ${__PSC_SETTINGS__+x} ]; then # include guard
+__PSC_SETTINGS__=1
+
+# EDIT at each rebuild of the software stack
+DATE_TAG="2023.01"
+
+if [ -z ${INSTALL_PREFIX+x} ]; then
+    INSTALL_PREFIX="/software/joey/${DATE_TAG}"
+fi
+
+if [ "${INSTALL_PREFIX%$DATE_TAG}" = "${INSTALL_PREFIX}" ]; then
+    echo "The path in 'INSTALL_PREFIX' must end with ${DATE_TAG} but its value is ${INSTALL_PREFIX}"
+    exit 1
+fi
+
+if [ -n "${PAWSEY_CLUSTER}" ] && [ -z ${SYSTEM+x} ]; then
+    SYSTEM="$PAWSEY_CLUSTER"
+fi
+
+if [ -z ${SYSTEM+x} ]; then
+    echo "The 'SYSTEM' variable is not set. Please specify the system you want to
+    build Spack for."
+    exit 1
+fi
+
+if [ "$USER" = "spack" ]; then
+    INSTALL_GROUP="spack"
+fi
+
+if [ -z ${INSTALL_GROUP+x} ]; then
+    echo "The 'INSTALL_GROUP' variable must be set to linux group that will own the installed files."
+    exit 1
+fi
+
+# Note the use of '' instead of "" to allow env variables to be present in config files
+USER_PERMANENT_FILES_PREFIX='/software/projects'
+USER_TEMP_FILES_PREFIX='/scratch'
+SPACK_USER_CONFIG_PATH="$MYSOFTWARE/joey/.spack_user_config"
+BOOTSTRAP_PATH='$MYSOFTWARE/joey/.spack_user_config/bootstrap'
+
+pawseyenv_version="${DATE_TAG}"
+
+archs="zen3"
+# compiler versions (needed for module trees with compiler dependency)
+gcc_version="12.1.0"
+cce_version="14.0.3"
+aocc_version="3.2.0"
+
+# architecture of login/compute nodes (needed by Singularity symlink module)
+cpu_arch="zen3"
+
+# tool versions
+spack_version="0.19.0" # the prefix "v" is added in setup_spack.sh
+singularity_version="3.8.6" # has to match the version in the Spack env yaml
+shpc_version="0.0.57"
+
+# python (and py tools) versions
+python_name="python"
+python_version="3.9.15" # has to match the version in the Spack env yaml
+setuptools_version="57.4.0" # has to match the version in the Spack env yaml
+pip_version="22.2.2" # has to match the version in the Spack env yaml
+# r major minor version
+r_version_majorminor="4.1"
+
+# list of module categories
+module_cat_list="
+astro-applications
+bio-applications
+applications
+libraries
+programming-languages
+utilities
+visualisation
+python-packages
+benchmarking
+developer-tools
+dependencies
+"
+
+# list of spack build environments - missing env_vis
+env_list="
+env_utils
+env_num_libs
+env_python
+env_io_libs
+env_langs
+env_apps
+env_devel
+env_bench
+env_s3_clients
+env_astro
+env_bio
+env_roms
+env_wrf
+"
+
+### TYPICALLY NO EDIT NEEDED PAST THIS POIINT
+
+# python version info (no editing needed)
+python_version_major="$( echo $python_version | cut -d '.' -f 1 )"
+python_version_minor="$( echo $python_version | cut -d '.' -f 2 )"
+python_version_bugfix="$( echo $python_version | cut -d '.' -f 3 )"
+
+# shpc module directories for all users (system-wide and user-specific)
+shpc_allusers_modules_dir_long="modules-long"
+shpc_allusers_modules_dir_short="views/modules"
+# shpc module suffix for SPACK USER (system-wide)
+shpc_spackuser_container_tag="-container"
+# name of SHPC module: decide this once and for all
+shpc_name="shpc"
+
+container_list="
+quay.io/biocontainers/bamtools:2.5.1--hd03093a_10
+quay.io/biocontainers/bbmap:38.96--h5c4e2a8_0
+quay.io/biocontainers/bcftools:1.15--haf5b3da_0
+quay.io/biocontainers/bedtools:2.30.0--h468198e_3
+quay.io/biocontainers/blast:2.12.0--pl5262h3289130_0
+quay.io/biocontainers/bowtie2:2.4.5--py36hd4290be_0
+quay.io/biocontainers/bwa:0.7.17--h7132678_9
+quay.io/biocontainers/bwa-mem2:2.2.1--hd03093a_2
+quay.io/biocontainers/canu:2.2--ha47f30e_0
+quay.io/biocontainers/clustalo:1.2.4--h87f3376_5
+quay.io/biocontainers/cutadapt:3.7--py38hbff2b2d_0
+quay.io/biocontainers/diamond:2.0.14--hdcc8f71_0
+quay.io/biocontainers/fastqc:0.11.9--hdfd78af_1
+quay.io/biocontainers/gatk4:4.2.5.0--hdfd78af_0
+quay.io/biocontainers/maker:3.01.03--pl526hb8757ab_0
+quay.io/biocontainers/mrbayes:3.2.7--h19cf415_2
+quay.io/biocontainers/mummer:3.23--pl5321h1b792b2_13
+quay.io/biocontainers/sambamba:0.8.1--h41abebc_0
+quay.io/biocontainers/samtools:1.15--h3843a85_0
+quay.io/biocontainers/spades:3.15.4--h95f258a_0
+quay.io/biocontainers/star:2.7.10a--h9ee0642_0
+quay.io/biocontainers/trimmomatic:0.39--hdfd78af_2
+quay.io/biocontainers/trinity:2.13.2--ha140323_0
+quay.io/biocontainers/vcftools:0.1.16--pl5321h9a82719_6
+quay.io/biocontainers/velvet:1.2.10--h7132678_5
+quay.io/pawsey/hpc-python:2022.03
+quay.io/pawsey/hpc-python:2022.03-hdf5mpi
+quay.io/pawsey/openfoam:v2012
+quay.io/pawsey/openfoam:v2006
+quay.io/pawsey/openfoam:v1912
+quay.io/pawsey/openfoam:v1812
+quay.io/pawsey/openfoam-org:8
+quay.io/pawsey/openfoam-org:7
+quay.io/sarahbeecroft9/alphafold:2.2.3
+quay.io/sarahbeecroft9/interproscan:5.56-89.0
+"
+# name of Singularity module (Spack has singularity and singularityce)
+singularity_name="singularity"
+
+# NOTE: the following are ALL RELATIVE to root_dir above
+# root location for Pawsey custom builds
+custom_root_dir="custom"
+# root location for Pawsey utilities (spack, shpc, scripts)
+utilities_root_dir="pawsey"
+# root location for containers
+containers_root_dir="containers"
+# location for Pawsey custom build modules
+custom_modules_dir="${custom_root_dir}/modules"
+# location for Pawsey custom build software
+custom_software_dir="${custom_root_dir}/software"
+# location for Pawsey utility modules
+utilities_modules_dir="${utilities_root_dir}/modules"
+# location for Pawsey utility software
+utilities_software_dir="${utilities_root_dir}/software"
+# location for SHPC container modules
+shpc_containers_modules_dir="${containers_root_dir}/${shpc_allusers_modules_dir_short}"
+# location for SHPC container modules (long version, as installed)
+shpc_containers_modules_dir_long="${containers_root_dir}/${shpc_allusers_modules_dir_long}"
+# location for SHPC containers
+shpc_containers_dir="${containers_root_dir}/sif"
+
+# suffix for Pawsey custom build modules
+custom_modules_suffix="custom"
+# suffix for Project modules
+project_modules_suffix=""   # "project-apps" # let us keep it standard vs spack
+# suffix for User modules
+user_modules_suffix=""   # "user-apps" # let us keep it standard vs spack
+
+# location of SHPC utility installation
+shpc_install_dir="${utilities_software_dir}/${shpc_name}"
+# location of SHPC utility modulefile
+shpc_module_dir="${utilities_modules_dir}/${shpc_name}"
+
+# location of Singularity modulefile (arch/compiler free symlink)
+singularity_symlink_module_dir="${utilities_modules_dir}/${singularity_name}"
+
+# location for Spack modulefile
+spack_module_dir="${utilities_modules_dir}/spack"
+
+fi # end include guard
---
+++ systems/joey/templates/modules/modulefile.lua
@@ -0,0 +1,161 @@
+-- -*- lua -*-
+-- Module file created by spack (https://github.com/spack/spack) on {{ timestamp }}
+--
+-- {{ spec.short_spec }}
+--
+
+{% block header %}
+{% if short_description %}
+whatis([[Name : {{ spec.name }}]])
+whatis([[Short description : {{ short_description }}]])
+whatis([[Version : {{ spec.version }}]])
+whatis([[Compiler : {{ spec.compiler }}]])
+whatis([[Flags : {{ spec.compiler_flags }}]])
+whatis([[Target : {{ spec.target }}]])
+whatis([[Build date : {{ spec.installation_time }}]])
+whatis([[Spack configuration : {{ spec.variants }}]])
+whatis([[Path : {{ spec.prefix }}]])
+{% endif %}
+{% if configure_options %}
+whatis([[Configure options : {{ configure_options }}]])
+{% endif %}
+
+{% if long_description %}
+help([[{{ long_description| textwrap(72)| join() }}]])
+{% endif %}
+{% endblock %}
+
+{% block provides %}
+{# Prepend the path I unlock as a provider of #}
+{# services and set the families of services I provide #}
+{% if has_modulepath_modifications %}
+-- Services provided by the package
+{% for name in provides %}
+family("{{ name }}")
+{% endfor %}
+
+-- Loading this module unlocks the path below unconditionally
+{% for path in unlocked_paths %}
+prepend_path("MODULEPATH", "{{ path }}")
+{% endfor %}
+
+{# Try to see if missing providers have already #}
+{# been loaded into the environment #}
+{% if has_conditional_modifications %}
+-- Try to load variables into path to see if providers are there
+{% for name in missing %}
+local {{ name }}_name = os.getenv("LMOD_{{ name|upper() }}_NAME")
+local {{ name }}_version = os.getenv("LMOD_{{ name|upper() }}_VERSION")
+{% endfor %}
+
+-- Change MODULEPATH based on the result of the tests above
+{% for condition, path in conditionally_unlocked_paths %}
+if {{ condition }} then
+  local t = pathJoin({{ path }})
+  prepend_path("MODULEPATH", t)
+end
+{% endfor %}
+
+-- Set variables to notify the provider of the new services
+{% for name in provides %}
+setenv("LMOD_{{ name|upper() }}_NAME", "{{ name_part }}")
+setenv("LMOD_{{ name|upper() }}_VERSION", "{{ version_part }}")
+{% endfor %}
+{% endif %}
+{% endif %}
+{% endblock %}
+
+{% block autoloads %}
+{% for module in autoload %}
+{% if verbose %}
+LmodMessage("Autoloading {{ module |replace("astro-applications/", "") |replace("bio-applications/", "") |replace("py-keras-applications/", "pawsey-temporary-string/") |replace("applications/", "") |replace("pawsey-temporary-string/", "py-keras-applications/") |replace("libraries/", "") |replace("programming-languages/", "") |replace("utilities/", "") |replace("visualisation/", "") |replace("python-packages/", "") |replace("benchmarking/", "") |replace("developer-tools/", "") |replace("dependencies/", "") |replace("project-apps/", "") |replace("user-apps/", "") }}")
+{% endif %}
+load("{{ module |replace("astro-applications/", "") |replace("bio-applications/", "") |replace("py-keras-applications/", "pawsey-temporary-string/") |replace("applications/", "") |replace("pawsey-temporary-string/", "py-keras-applications/") |replace("libraries/", "") |replace("programming-languages/", "") |replace("utilities/", "") |replace("visualisation/", "") |replace("python-packages/", "") |replace("benchmarking/", "") |replace("developer-tools/", "") |replace("dependencies/", "") |replace("project-apps/", "") |replace("user-apps/", "") }}")
+{% endfor %}
+{% endblock %}
+
+{% block environment %}
+{% for command_name, cmd in environment_modifications %}
+{% if command_name == 'PrependPath' %}
+prepend_path("{{ cmd.name }}", "{{ cmd.value }}", "{{ cmd.separator }}")
+{% elif command_name == 'AppendPath' %}
+append_path("{{ cmd.name }}", "{{ cmd.value }}", "{{ cmd.separator }}")
+{% elif command_name == 'RemovePath' %}
+remove_path("{{ cmd.name }}", "{{ cmd.value }}", "{{ cmd.separator }}")
+{% elif command_name == 'SetEnv' %}
+setenv("{{ cmd.name }}", "{{ cmd.value }}")
+{% elif command_name == 'UnsetEnv' %}
+unsetenv("{{ cmd.name }}")
+{% endif %}
+{% endfor %}
+{% endblock %}
+{% if spec.name == 'nextflow' %}setenv("NXF_HOME", os.getenv("MYSOFTWARE").."/.nextflow")
+setenv("NXF_SINGULARITY_CACHEDIR", os.getenv("MYSOFTWARE").."/.nextflow_singularity")
+{% endif %}
+{% if spec.name == 'openjdk' %}setenv("GRADLE_USER_HOME", os.getenv("MYSOFTWARE").."/.gradle")
+{% endif %}
+{% if spec.name == 'singularity' %}setenv("SINGULARITY_CACHEDIR", os.getenv("MYSOFTWARE").."/.singularity")
+-- MPI-CONFIG Singularity configuration 
+-- for Cassini nics and SS>=11
+-- LD_LIBRARY_PATH addition 
+-- add cray paths 
+local singularity_ld_path = "/opt/cray/pe/mpich/default/ofi/gnu/9.1/lib-abi-mpich:/opt/cray/pe/mpich/default/gtl/lib:/opt/cray/xpmem/default/lib64:/opt/cray/pe/pmi/default/lib:/opt/cray/pe/pals/default/lib"
+singularity_ld_path = singularity_ld_path .. ":/opt/cray/pe/gcc-libs:
+-- add libfabric 
+singularity_ld_path = singularity_ld_path .. ":/opt/cray/libfabric/1.15.0.0/lib64/"
+-- add local ld library path 
+singularity_ld_path = singularity_ld_path .. ":$LD_LIBRARY_PATH"
+setenv("SINGULARITYENV_LD_LIBRARY_PATH", singularity_ld_path)
+
+-- BIND_PATH addition
+-- bind paths of standard filesystems and specific cray libraries
+local singularity_bindpath = "/scratch,/software
+singularity_bindpath = singularity_bindpath .. ",/var/opt/cray/pe,/etc/opt/cray/pe,/opt/cray,/etc/alternatives/cray-dvs,/etc/alternatives/cray-xpmem"
+-- commented out below, adding lib64. Could be useful
+-- singularity_bindpath = singularity_bindpath .. ",/lib64/libc.so.6,/lib64/libpthread.so.0,/lib64/librt.so.1,/lib64/libdl.so.2,/lib64/libz.so.1,/lib64/libselinux.so.1,/lib64/libm.so.6"
+-- then bind networking libraries for mpi
+singularity_bindpath = singularity_bindpath .. ",/usr/lib64/libcxi.so.1,/usr/lib64/libcurl.so.4,/usr/lib64/libjson-c.so.3"
+singularity_bindpath = singularity_bindpath .. ",/usr/lib64/libnghttp2.so.14,/usr/lib64/libidn2.so.0,/usr/lib64/libssh.so.4,/usr/lib64/libpsl.so.5,/usr/lib64/libssl.so.1.1,/usr/lib64/libcrypto.so.1.1,/usr/lib64/libgssapi_krb5.so.2,/usr/lib64/libldap_r-2.4.so.2,/usr/lib64/liblber-2.4.so.2,/usr/lib64/libunistring.so.2,/usr/lib64/libkrb5.so.3,/usr/lib64/libk5crypto.so.3,/lib64/libcom_err.so.2,/usr/lib64/libkrb5support.so.0,/lib64/libresolv.so.2,/usr/lib64/libsasl2.so.3,/usr/lib64/libkeyutils.so.1,/usr/lib64/libpcre.so.1"
+singularity_bindpath = singularity_bindpath .. ",/usr/lib64/libmunge.so.2"
+-- this has to be conditional, path exists only in compute nodes
+if isDir("/var/spool/slurmd") then
+  singularity_bindpath = singularity_bindpath .. ",/var/spool/slurmd"
+  singularity_bindpath = singularity_bindpath .. ",/var/run/munge"
+end
+setenv("SINGULARITY_BINDPATH",singularity_bindpath)
+
+-- LD_PRELOAD addition 
+-- preload xpmem for fast mpi communication
+local singularity_ld_preload = "/opt/cray/xpmem/default/lib64/libxpmem.so.0"
+-- singularity_ld_preload = singularity_ld_preload .. ":/lib64/libc.so.6:/lib64/libpthread.so.0:/lib64/librt.so.1:/lib64/libdl.so.2:/lib64/libz.so.1:/lib64/libselinux.so.1:/lib64/libm.so.6"
+singularity_ld_preload = singularity_ld_preload .. ":/usr/lib64/libcxi.so.1:/usr/lib64/libcurl.so.4:/usr/lib64/libjson-c.so.3"
+singularity_ld_preload = singularity_ld_preload .. ":/usr/lib64/libnghttp2.so.14:/usr/lib64/libidn2.so.0:/usr/lib64/libssh.so.4:/usr/lib64/libpsl.so.5:/usr/lib64/libssl.so.1.1:/usr/lib64/libcrypto.so.1.1:/usr/lib64/libgssapi_krb5.so.2:/usr/lib64/libldap_r-2.4.so.2:/usr/lib64/liblber-2.4.so.2:/usr/lib64/libunistring.so.2:/usr/lib64/libkrb5.so.3:/usr/lib64/libk5crypto.so.3:/lib64/libcom_err.so.2:/usr/lib64/libkrb5support.so.0:/lib64/libresolv.so.2:/usr/lib64/libsasl2.so.3:/usr/lib64/libkeyutils.so.1:/usr/lib64/libpcre.so.1"
+singularity_ld_preload = singularity_ld_preload .. ":/usr/lib64/libmunge.so.2"
+prepend_path("SINGULARITYENV_LD_PRELOAD", singularity_ld_preload)
+
+{% endif %}
+{% if spec.name == 'r' %}setenv("R_LIBS_USER", os.getenv("MYSOFTWARE").."/joey/r/%v")
+{% endif %}
+{% if spec.name == 'python' %}setenv("PYTHONUSERBASE", os.getenv("MYSOFTWARE").."/joey/python")
+prepend_path("PATH", os.getenv("PYTHONUSERBASE").."/bin")
+{% endif %}
+
+{% block footer %}
+-- Enforce explicit usage of versions by requiring full module name
+if (mode() == "load") then
+  if (myModuleUsrName() ~= myModuleFullName()) then
+    LmodError(
+        "Default module versions are disabled by your systems administrator.\n\n",
+        "\tPlease load this module as <name>/<version>.\n"
+    )
+  end
+end
+
+-- Access is granted only to specific groups
+if not isDir("{{ spec.prefix }}") then
+    LmodError (
+        "You don't have the necessary rights to run \"{{ spec.name }}\".\n\n",
+        "\tPlease raise a help ticket if you need further information on how to get access to it.\n"
+    )
+end
+{% endblock %}
