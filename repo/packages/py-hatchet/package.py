8b4d71f2fce17cc26f0a120e7cc61351713d996e
Deva Deeptimahanti <ddeeptimahanti@setonix-01.pawsey.org.au>
2023-06-21 10:09:33 +0800

    Upgraded software versions that are currently supported and preferred in Spack v0.19.0

--- repo/packages/py-hatchet/package.py
+++ repo/packages/py-hatchet/package.py
@@ -11,26 +11,25 @@ class PyHatchet(PythonPackage):
     using a graph-indexed Pandas dataframe."""
 
     homepage = "https://github.com/hatchet/hatchet"
-    url      = "https://github.com/hatchet/hatchet/archive/v1.0.0.tar.gz"
-    tags     = ['radiuss']
+    url = "https://files.pythonhosted.org/packages/be/75/904c27e3d96d73d5743257caaf820564b0ac3be3497ec2765200ed5241f9/hatchet-1.3.1.tar.gz"
+    tags = ["radiuss"]
 
     maintainers = ["slabasan", "bhatele", "tgamblin"]
 
-    version('1.3.1a0', sha256='ce7f39c8dcf2a6e02493ca27b2e4614ad7401aabcaabb4eb72cf1554e2fdae4a')
-    version('1.3.0', sha256='d77d071fc37863fdc9abc3fd9ea1088904cd98c6980a014a31e44595d2deac5e')
-    version('1.2.0', sha256='1d5f80abfa69d1a379dff7263908c5c915023f18f26d50b639556e2f43ac755e')
-    version('1.1.0', sha256='71bfa2881ef295294e5b4493acb8cce98d14c354e9ae59b42fb56a76d8ec7056')
-    version('1.0.1', sha256='e5a4b455ab6bfbccbce3260673d9af8d1e4b21e19a2b6d0b6c1e1d7727613b7a')
-    version('1.0.0', sha256='efd218bc9152abde0a8006489a2c432742f00283a114c1eeb6d25abc10f5862d')
+    version("1.3.1", sha256="17b36abd4bc8c6d5fed452267634170159062ca3c2534199b1c8378f2f3f1f28")
+    version("1.3.0", sha256="d77d071fc37863fdc9abc3fd9ea1088904cd98c6980a014a31e44595d2deac5e")
+    version("1.2.0", sha256="1d5f80abfa69d1a379dff7263908c5c915023f18f26d50b639556e2f43ac755e")
+    version("1.1.0", sha256="71bfa2881ef295294e5b4493acb8cce98d14c354e9ae59b42fb56a76d8ec7056")
+    version("1.0.1", sha256="e5a4b455ab6bfbccbce3260673d9af8d1e4b21e19a2b6d0b6c1e1d7727613b7a")
+    version("1.0.0", sha256="efd218bc9152abde0a8006489a2c432742f00283a114c1eeb6d25abc10f5862d")
 
     # https://github.com/hatchet/hatchet/issues/428
-    depends_on('python@2.7:3.8', when='@:1.3.0', type=('build', 'run'))
-    depends_on('python@2.7:', when='@1.3.1:', type=('build', 'run'))
+    depends_on("python@2.7:3.10.15", when="@:1.3.0", type=("build", "run"))
+    depends_on("python@2.7:", when="@1.3.1:", type=("build", "run"))
 
-    depends_on('py-setuptools', type='build')
-    depends_on('py-matplotlib', type=('build', 'run'))
-    depends_on('py-numpy',      type=('build', 'run'))
-    depends_on('py-pandas',     type=('build', 'run'))
-    depends_on('py-pydot',      type=('build', 'run'))
-    depends_on('py-pyyaml',     type=('build', 'run'))
-    depends_on('py-multiprocess', when='@1.3.1:', type=('build', 'run'))
+    depends_on("py-setuptools", type="build")
+    depends_on("py-matplotlib", type=("build", "run"))
+    depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-pandas", type=("build", "run"))
+    depends_on("py-pydot", type=("build", "run"))
+    depends_on("py-pyyaml", type=("build", "run"))
