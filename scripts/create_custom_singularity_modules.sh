db782927245db4e04f81ca316ef7341ad4ae246f
Pascal Elahi <pascaljelahi@gmail.com>
2023-06-22 14:01:18 +0800

    Update to singularity modules production
    Script that produces multiple modules is updated to auto produce several
    different modules: askap, mpi, nompi, nohost and finally a new one slurm
    
    The module template has been updated to include several more libraries
    required by the newer libfabric. Also adds lustre.

--- scripts/create_custom_singularity_modules.sh
+++ scripts/create_custom_singularity_modules.sh
@@ -17,15 +17,12 @@ PAWSEY_SPACK_CONFIG_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /d
 
 # This script creates end user singularity modules, starting from the Spack one
 # For each version:
-# 1. creates 3 pawsey/modules : 
-#    A. singularity-nompi
-#    B. singularity-mpi
-#    C. singularity-astro
-#    The purpose are:
-#    - to have Singularity available regardless the loaded PrgEng/compiler
-#      (works thanks to RPATHs)
-#    - to have /astro and /askapbuffer only for astronomers
-#    - to have MPI bind mounts only when needed
+# 1. creates several modules each setting different default behaviour:
+#    A. singularity/x.x.x-mpi : injects all necessary libraries to run MPI, binds filesystems /scratch, /software, etc
+#    B. singularity/x.x.x-nompi : binds filesystems, also sets LD path based on currenty LD path 
+#    C. singularity/x.x.x-askap : like MPI but also addes /askapbuffer and also adds slurm related bind mounts 
+#    D. singularity/x.x.x-nohost : nothing from the host is injected, does mount file systems
+#    E. singularity/x.x.x-slurm : like nompi but also mounts directories related to running slurm in a container
 # 2. hides Spack module in {spack modules}/{arch}/{compiler}
 #    - to avoid confusion
 
@@ -43,23 +40,32 @@ for version in $( ls ${src_dir}/*.lua ${src_dir}/.*.lua 2>/dev/null ) ; do
   version="${version#.}"
   #2. hide Spack module
   if [ -e ${src_dir}/${version} ] ; then
-    mv ${src_dir}/${version} ${src_dir}/.${version}
+    mv ${src_dir}/${version}.lua ${src_dir}/.${version}
   fi
-  # 1.C singularity-astro is just the original module
-  cp -p ${src_dir}/.${version} ${dst_dir}/${version/\.lua/-astro.lua}
-  # 1.B singularity-mpi does not bind mount /askapbuffer and /astro
+  # 1.C singularity-askap is just the original module
+  cp -p ${src_dir}/.${version} ${dst_dir}/${version}-askap.lua
+  # 1.A singularity does not bind mount /askapbuffer nor add slurm
   sed \
     -e '/singularity_bindpath *=/ s;/askapbuffer;;g' \
-    -e '/singularity_bindpath *=/ s;/astro;;g' \
-    ${dst_dir}/${version/\.lua/-astro.lua} \
-    >${dst_dir}/${version/\.lua/-mpi.lua}
-  # 1.A singularity-nompi does not bind mount MPI-specific paths
+    -e '/^-- add SLURM START/,/^-- add SLURM END/{/^-- add SLURM START/!{/^-- add SLURM END/!d}}' \
+    ${src_dir}/.${version} > ${dst_dir}/${version}-mpi.lua
+  # 1.B singularity does not add any mpi related stuff
   sed \
-    -e '/MPI + Singularity configuration/,/singularity_ld_path *= *singularity_ld_path/d' \
-    -e 's;, *singularity_ld_path;,"$LD_LIBRARY_PATH";' \
-    -e 's;,/etc/alternatives/cray-xpmem;;' \
-    -e '/singularity_bindpath.*libselinux/,/singularity_bindpath.*libmunge/d' \
-    -e '/LD_PRELOAD edited/,/, *singularity_ld_preload/d' \
-    ${dst_dir}/${version/\.lua/-mpi.lua} \
-    >${dst_dir}/${version/\.lua/-nompi.lua}
+    -e '/singularity_bindpath *=/ s;/askapbuffer;;g' \
+    -e '/^-- add MPI START/,/^-- add MPI END/{/^-- add MPI START/!{/^-- add MPI END/!d}}' \
+    -e '/^-- add SLURM START/,/^-- add SLURM END/{/^-- add SLURM START/!{/^-- add SLURM END/!d}}' \
+    ${src_dir}/.${version} > ${dst_dir}/${version}-nompi.lua
+  # 1.D singularity does not add any thing at all from host 
+  sed \
+    -e '/singularity_bindpath *=/ s;/askapbuffer;;g' \
+    -e '/^-- add MPI START/,/^-- add MPI END/{/^-- add MPI START/!{/^ add MPI END/!d}}' \
+    -e '/^-- add SLURM START/,/^-- add SLURM END/{/^-- add SLURM START/!{/^-- add SLURM END/!d}}' \
+    -e '/^-- add CRAY_PATHS START/,/^-- add CRAY_PATHS END/{/^-- add CRAY_PATHS START/!{/^-- add CRAY_PATHS END/!d}}' \
+    -e '/^-- add CURRENT_HOST_LD_PATH START/,/^-- add CURRENT_HOST_LD_PATH END/{/^-- add CURRENT_HOST_LD_PATH START/!{/^-- add CURRENT_HOST_LD_PATH END/!d}}' \
+    ${src_dir}/.${version} > ${dst_dir}/${version}-nohost.lua 
+  # 1.B singularity does not add any mpi related stuff
+  sed \
+    -e '/singularity_bindpath *=/ s;/askapbuffer;;g' \
+    -e '/^-- add MPI START/,/^-- add MPI END/{/^-- add MPI START/!{/^-- add MPI END/!d}}' \
+    ${src_dir}/.${version} > ${dst_dir}/${version}-slurm.lua
 done
