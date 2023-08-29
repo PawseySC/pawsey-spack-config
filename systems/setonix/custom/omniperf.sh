1dbd53f6f9e0ef35305616cc02b5c396b1555946
ilkhom <ilkhom@pawsey.org.au>
2023-08-29 16:00:27 +0800

    adding installation script for omniperf

--- systems/setonix/custom/omniperf.sh
+++ systems/setonix/custom/omniperf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-module load python/3.10.8 
-module load py-pip/22.2.2-py3.10.8
+module load python/3.10.10 
+module load py-pip/23.1.2-py3.10.10
 module load cmake/3.21.4
 wget https://github.com/AMDResearch/omniperf/releases/download/v1.0.6/omniperf-v1.0.6.tar.gz
 tar -xf omniperf-v1.0.6.tar.gz
@@ -20,4 +20,4 @@ cd ../..
 rm omniperf-v1.0.6.tar.gz
 rm -rf omniperf-1.0.6
 cd ${MODULE_DIR}/omniperf
-sed -i -e '$adepends_on("python/3.10.8")' 1.0.6.lua
+sed -i -e '$adepends_on("python/3.10.10")' 1.0.6.lua
