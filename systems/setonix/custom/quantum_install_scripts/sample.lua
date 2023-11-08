288003c265856e9abbe41fb8c6d48f291e6997a6
Pascal Elahi <pascaljelahi@gmail.com>
2023-11-08 15:40:51 +0800

    Updated the sample.lua
    - Removed unnecessary python path updates
    - also added +x to the uber install script

--- systems/setonix/custom/quantum_install_scripts/sample.lua
+++ systems/setonix/custom/quantum_install_scripts/sample.lua
@@ -22,8 +22,6 @@ prepend_path("CMAKE_PREFIX_PATH", "INSTALL_PATH/", ":")
 prepend_path("PATH", "INSTALL_PATH/bin", ":")
 prepend_path("CMAKE_PREFIX_PATH", "INSTALL_PATH/", ":")
 prepend_path("PYTHONPATH", "INSTALL_PATH/lib/python3.10/site-packages", ":")
--- prepend_path("PYTHONPATH", "/software/setonix/2023.08/software/linux-sles15-zen3/gcc-12.2.0/py-setuptools-59.4.0-x3ywpd6otzgbfcyp67iw67ckibsdqrnq/lib/python3.10/site-packages", ":")
--- setenv("PAWSEY_PY_NUMPY_HOME", "/software/setonix/2023.08/software/linux-sles15-zen3/gcc-12.2.0/py-numpy-1.23.4-tfvpg5jilocjyzthio3hqjlguab44lwt")
 
 -- Enforce explicit usage of versions by requiring full module name
 if (mode() == "load") then
