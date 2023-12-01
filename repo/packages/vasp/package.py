87daad41d69a21eb00b336cb4bda9bd9c442b721
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2023-12-01 13:39:58 +0800

    Lists Pawsey additions to the VASP recipe.

--- repo/packages/vasp/package.py
+++ repo/packages/vasp/package.py
@@ -2,7 +2,7 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
+# Pawsey additions: vaspsol, vtst and dftd4 support.
 import os
 import grp
 import shutil
