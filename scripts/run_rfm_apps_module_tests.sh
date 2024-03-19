d477eacc83807e33176e812368b586cc4eab08f3
Craig Meyer <cmeyer@setonix-09.pawsey.org.au>
2024-03-19 14:34:13 +0800

    Add scripts for running Reframe tests

---
+++ scripts/run_rfm_apps_module_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Run Reframe module tests across the apps environment ONLY
+# Since many apps packages require manual steps after automatic pipeline
+# it is not beneficial to include those tests during installation as many
+# will always fail. Therefore, this script is intended to be run after the
+# apps packages have been installed fully.
+
+if [ -n "${PAWSEY_CLUSTER}" ] && [ -z ${SYSTEM+x} ]; then
+    SYSTEM="$PAWSEY_CLUSTER"
+fi
+
+if [ -z ${SYSTEM+x} ]; then
+    echo "The 'SYSTEM' variable is not set. Please specify the system you want to
+    build Spack for."
+    exit 1
+fi
+
+PAWSEY_SPACK_CONFIG_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
+. "${PAWSEY_SPACK_CONFIG_REPO}/systems/${SYSTEM}/settings.sh"
+
+# Add node this job is running on to host list of ReFrame, allowing it to run from this node
+sed -i "s/\(hostnames.*setonix-01.*\).*\(\]\)/\1,'${SLURM_JOB_NODELIST}'\2/" ${RFM_SETTINGS_FILE}
+
+# Run the Reframe module tests for the apps environment
+echo "Running ReFrame tests for modules in env apps"
+export SPACK_ENV=apps
+reframe -C ${RFM_SETTINGS_FILE} -c ${RFM_TEST_FILE} --prefix=${RFM_STORAGE_DIR} --report-file=${RFM_STORAGE_DIR}/rfm_install_report_apps.json -t installation -r
+unset SPACK_ENV
+mv reframe.out ${RFM_STORAGE_DIR}/reframe_apps_install.out
+mv reframe.log ${RFM_STORAGE_DIR}/reframe_apps_install.log
+
+# Reset valid setonix hostnames to original entries (i.e. remove the node this job runs on) for future runs
+sed -i "s/\(hostnames.*setonix-01.*\)\(,.*\]\)/\1]/" ${RFM_SETTINGS_FILE}
