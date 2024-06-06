84eed0a2f3517b6762e9037774ab9e2c211fd3c3
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2024-06-06 22:29:47 +0800

    Build wsclean@2.9 and @2.10.1 on Setonix
    Temporarily removes all the 3.x stuff. I will reintroduce them step by step
    as I try 3.x builds but at least 2.x builds fine.

--- repo/packages/wsclean/package.py
+++ repo/packages/wsclean/package.py
@@ -2,58 +2,54 @@
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-# contribute
-from spack.package import *
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install cotter
+#
+# You can edit this file again by typing:
+#
+#     spack edit cotter
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
 
 
 class Wsclean(CMakePackage):
-    """
-    WSClean (w-stacking clean) is a fast generic widefield imager. It uses the w-stacking algorithm 
-    and can make use of the w-snapshot algorithm. As of Feb 2014, it is 2-12 times faster than CASA's 
-    w-projection, depending on the array configuration. It supports full-sky imaging and proper beam 
-    correction for homogeneous dipole arrays such as the MWA.
-    WSClean allows Hogbom and Cotton-Schwab cleaning and has wideband, multiscale, compressed 
-    sensing and joined-polarization deconvolution modes. All operations are performed on the CPU.
-    """
+    """FIXME: Put a proper description of your package here."""
 
-    homepage = "https://wsclean.readthedocs.io/en/latest/"
-    git      = "https://gitlab.com/aroffringa/wsclean.git"
+    homepage = "https://gitlab.com/aroffringa/wsclean"
 
-    version('master', branch='master')
-    version('3.1', commit='ea18d0139e35050d58b2758cf5015539f3e2d870') 
-    version('3.0.1', commit='1a4e5928689b23d3034549c2541829427d91fa8e')
-    version('3.0', commit='9ee587c576caad779dc127bb3f83858513679333')
-    version('2.10.1', commit='6567e4891467c23a27391b49273b3fbb94c45831')
-    version('2.10', commit='7b82d8c19ccb09cf2d7872a338d8266d09dd0481')
+    maintainers = ['dipietrantonio']
 
-    variant('idg', default=True, description='To enable Image Domain Gridder (a fast GPU-enabled gridder) for version >=3')
-    variant('everybeam', default=False, description='To apply primary beams for version >=3')
-    variant('mpi', default=False, description='To enable distributed mode')
+    version('3.4', git='https://gitlab.com/aroffringa/wsclean.git', tag='v3.4', submodules=True)
+    version('2.10.1', git='https://gitlab.com/aroffringa/wsclean.git', tag='v2.10.1', submodules=True)
+    version('2.9', git='https://gitlab.com/aroffringa/wsclean.git', tag='wsclean2.9', submodules=True)
 
-    depends_on('chgcentre', type='build')
-    depends_on('casacore')
-    depends_on('fftw-api@3')
-    depends_on('gsl')
+    depends_on('casacore@3.2.1:')
+    depends_on('fftw@3.3.8:')
+    depends_on('hdf5@1.10.7 +cxx ~mpi api=v110')
+    depends_on('gsl@2.6:')
     depends_on('cfitsio')
-    depends_on('hdf5 +cxx ~mpi api=v110', when='@2.10.1')
-    depends_on('boost')
-    depends_on('idg@0.8.1', when='@3.0 +idg')
-    depends_on('idg@1.0.0', when='@3.1 +idg')
-    depends_on('everybeam@0.2.0', when='@3.0 +everybeam')
-    depends_on('everybeam@0.3.1', when='@3.1 +everybeam')
-    depends_on('mpi', when='+mpi')
-    depends_on('blas', when='@3.0:')
-    depends_on('doxygen', when='@3.0:')
-    depends_on('python', when='@3.0:')
-    patch('cmake.patch', when='@3.0:')
-    patch('mpi1.patch', when='@3.0:')
-    patch('mpi2.patch', when='@3.0:')
-    patch('cmake.for.v2.0.patch', when='@2.10.1')
+    depends_on('boost@1.80.0 +date_time +filesystem +system +test +program_options')
+    patch("wsclean_2.10.1.patch", when="@2.10.1")
+
+    @run_before("cmake")
+    def change_source_dir(self):
+        if self.spec.version == Version("2.9"):
+            self.__class__.root_cmakelists_dir = "wsclean"
+
 
     def cmake_args(self):
-        args = []
-        spec = self.spec
-        args.append(self.define_from_variant('USE_MPI', 'mpi'))
-
+        args = [
+            f"-DHDF5_LIBRARIES={self.spec['hdf5'].prefix.lib}/libhdf5_cpp.so"
+        ]
         return args
-
