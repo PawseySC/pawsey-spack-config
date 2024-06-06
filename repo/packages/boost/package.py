76301e4aad8115dea104fa24ab188c6a71ad4588
Craig Meyer <cmeyer@setonix-05.pawsey.org.au>
2024-06-06 14:39:36 +0800

    Boost package.py file newline test

--- repo/packages/boost/package.py
+++ repo/packages/boost/package.py
@@ -775,3 +775,4 @@ class Boost(Package):
                 env.append_flags("CXXFLAGS", "-DBOOST_USE_UCONTEXT")
             elif context_impl == "winfib":
                 env.append_flags("CXXFLAGS", "-DBOOST_USE_WINFIB")
+
