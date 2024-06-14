a4650e6dad9bb893b6090314b288d089aaaacf91
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2024-06-14 10:41:52 +0800

    Reverts to cfitsio@3.49 - @4 breaks stuff.

--- repo/packages/birli/package.py
+++ repo/packages/birli/package.py
@@ -11,7 +11,7 @@ class Birli(Package):
     version("0.10.0", tag="v0.8.0")
 
     depends_on("rust@1.64.0:", type="build")
-    depends_on("cfitsio@3.49:")
+    depends_on("cfitsio@3.49")
     depends_on("curl") # because cfitsio does not --disable-curl by default
     depends_on("aoflagger@3.2.0:")
     depends_on("erfa") # because of Marlu
