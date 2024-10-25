6f8a1db8dc1b015c19b09bbf8939392c30df7f4c
Edric-Matwiejew <edric.matviev@gmail.com>
2024-10-25 09:37:54 +0800

    CRAYPE_VERSION check for cray-specific commands, spack loads gcc if "${gcc_version}" is not in PATH

--- scripts/concretize_environments.sh
+++ scripts/concretize_environments.sh
@@ -16,10 +16,18 @@ PAWSEY_SPACK_CONFIG_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /d
 module use ${INSTALL_PREFIX}/staff_modulefiles
 # we need the python module to be available in order to run spack
 module --ignore-cache load pawseyenv/${pawseyenv_version}
-# swap is needed for the pawsey_temp module to work
-module swap PrgEnv-gnu PrgEnv-cray
-module swap PrgEnv-cray PrgEnv-gnu
+
+if [ ! -z "${CRAYPE_VERSION+x}" ]; then
+    # swap is needed for the pawsey_temp module to work
+    module swap PrgEnv-gnu PrgEnv-cray
+    module swap PrgEnv-cray PrgEnv-gnu
+fi
+
 module load spack/${spack_version}
+
+if ! $(gcc -v 2>&1 | grep -q "${gcc_version}") ; then
+	spack load gcc@${gcc_version}
+fi
 
 # list of environments included in variables.sh (sourced above)
 envdir="${PAWSEY_SPACK_CONFIG_REPO}/systems/${SYSTEM}/environments"
