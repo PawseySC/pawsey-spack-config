870ae30a939fe0875dea211444cb678c5ef8b23a
ilkhom <ilkhom@pawsey.org.au>
2024-11-18 22:42:29 +0800

    utils fixed

--- scripts/concretize_environments.sh
+++ scripts/concretize_environments.sh
@@ -13,12 +13,16 @@ fi
 PAWSEY_SPACK_CONFIG_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 . "${PAWSEY_SPACK_CONFIG_REPO}/systems/${SYSTEM}/settings.sh"
 
+# swap is needed for the pawsey_temp module to work
+module swap PrgEnv-gnu PrgEnv-cray
+module swap PrgEnv-cray PrgEnv-gnu
 module use ${INSTALL_PREFIX}/staff_modulefiles
 # we need the python module to be available in order to run spack
 module --ignore-cache load pawseyenv/${pawseyenv_version}
 # swap is needed for the pawsey_temp module to work
 module swap PrgEnv-gnu PrgEnv-cray
 module swap PrgEnv-cray PrgEnv-gnu
+module use ${INSTALL_PREFIX}/modules/zen3/gcc/13.3.0/programming-languages
 module load spack/${spack_version}
 
 # list of environments included in variables.sh (sourced above)
