c5a46706a8049256fa139853e988b8e1403a301e
Greg Sleap <greg.sleap@curtin.edu.au>
2025-03-20 17:11:06 +1100

    Updated order and rust dependencies based on PR feedback

--- repo/packages/giant-squid/package.py
+++ repo/packages/giant-squid/package.py
@@ -15,11 +15,12 @@ class GiantSquid(Package):
 
     maintainers = ["gsleap", "d3v-null"]
 
+    version("2.0.1", tag="v2.0.1")
     version("1.1.0", tag="v1.1.0")
     version("1.0.3", tag="v1.0.3")
-    version("2.0.1", tag="v2.0.1")
 
-    depends_on("rust@1.71.1:", type="build")
+    depends_on("rust@1.71.1:", when="@1.1.0:", type="build")
+    depends_on("rust@1.70.0:", when="@:1.0.3", type="build")
 
     def setup_build_environment(self, env):
         build_dir = self.stage.source_path
