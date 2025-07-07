04375153ac203e92c953a19bcb5285dd789dfc0c
Ilkhom Abdurakhmanov <ilkhom@pawsey.org.au>
2025-07-07 15:29:14 +0800

    fixing x264 package

--- systems/setonix/settings.sh
+++ systems/setonix/settings.sh
@@ -3,7 +3,7 @@ if [ -z ${__PSC_SETTINGS__+x} ]; then # include guard
 __PSC_SETTINGS__=1
 
 # EDIT at each rebuild of the software stack
-DATE_TAG="2025.06"
+DATE_TAG="2025.08"
 
 if [ -z ${INSTALL_PREFIX+x} ]; then
     INSTALL_PREFIX="/software/setonix/${DATE_TAG}"
