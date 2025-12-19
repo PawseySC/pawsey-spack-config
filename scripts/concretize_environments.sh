7d4d3d2e6accfbe414f692f0ba8cc3534898dcc0
Pascal Jahan Elahi <pascaljelahi@gmail.com>
2025-12-19 16:27:34 +0800

    some updates to default packages

--- scripts/concretize_environments.sh
+++ scripts/concretize_environments.sh
@@ -1,17 +1,9 @@
 #!/bin/bash -e
 
-if [ -n "${PAWSEY_CLUSTER}" ] && [ -z ${SYSTEM+x} ]; then
-    SYSTEM="$PAWSEY_CLUSTER"
-fi
-
-if [ -z ${SYSTEM+x} ]; then
-    echo "The 'SYSTEM' variable is not set. Please specify the system you want to
-    build Spack for."
-    exit 1
-fi
-
-PAWSEY_SPACK_CONFIG_REPO=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
-. "${PAWSEY_SPACK_CONFIG_REPO}/systems/${SYSTEM}/settings.sh"
+check_installation_environment
+set_spack_config_repo
+set_compilation_sets_for_arch
+. "${INSTALL_PREFIX}/spack/share/spack/setup-env.sh"
 
 # swap is needed for the pawsey_temp module to work
 module swap PrgEnv-gnu PrgEnv-cray
@@ -20,11 +12,11 @@ module load cpe/25.03
 module use ${INSTALL_PREFIX}/staff_modulefiles
 # we need the python module to be available in order to run spack
 module --ignore-cache load pawseyenv/${pawseyenv_version}
-module load gcc-native/14.2
+module load gcc-native/${gcc_version}
 # swap is needed for the pawsey_temp module to work
 #module swap PrgEnv-gnu PrgEnv-cray
 #module swap PrgEnv-cray PrgEnv-gnu
-module use ${INSTALL_PREFIX}/modules/zen3/gcc/14.2.0/programming-languages
+module use ${INSTALL_PREFIX}/modules/${mainarch}/gcc/${gcc_version}/programming-languages
 module load spack/${spack_version}
 
 # list of environments included in variables.sh (sourced above)
