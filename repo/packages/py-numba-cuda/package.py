d75d60d5d66101799793bf930dc0e5bc84b44df8
Edric-Matwiejew <edric.matwiejew@research.uwa.edu.au.com>
2026-01-23 18:46:34 +0800

    simplify cuda depends

--- repo/packages/py-numba-cuda/package.py
+++ repo/packages/py-numba-cuda/package.py
@@ -8,6 +8,8 @@ class PyNumbaCuda(PythonPackage):
     pypi = "numba_cuda/numba_cuda-0.24.0.tar.gz"
 
     version("0.24.0", sha256="f15a11a8d224e160e9cb2345049c5fa0bbd49eb255b2e8f661086746a7feb378")
+    # Alias for environments requesting 0.24
+    version("0.24", sha256="f15a11a8d224e160e9cb2345049c5fa0bbd49eb255b2e8f661086746a7feb378", deprecated=True)
 
     depends_on('python@3.7:', type=('build', 'run'))
     depends_on('py-setuptools', type='build')
