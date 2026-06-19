dd3d698c1eaccf27804174ef0c4ef43833398890
Edric Matwiejew <edric.matwiejew@CSIRO.au>
2026-06-19 14:58:58 +0800

    Removed zen3 reference, make variable generic.

--- scripts/create_wrf_roms_dependency_modules.sh
+++ scripts/create_wrf_roms_dependency_modules.sh
@@ -6,15 +6,16 @@ set_compilation_sets_for_arch
 set_modulepaths_for_arch
 
 # create destination directory
-custom_full_modules_dir_zen3_gcc="${INSTALL_PREFIX}/${custom_modules_dir}/zen3/gcc/${gcc_version}/${custom_modules_suffix}"
-mkdir -p "${custom_full_modules_dir_zen3_gcc}"
+custom_full_modules_dir_gcc="${INSTALL_PREFIX}/${custom_modules_dir}/${mainarch}/gcc/${gcc_version}/${custom_modules_suffix}"
+mkdir -p "${custom_full_modules_dir_gcc}"
 
 ENVS_DIR="${PAWSEY_SPACK_CONFIG_REPO}/systems/${SYSTEM}/environments"
 
 # wrf dependency module
 for pkg in wrf roms;
 do
-    "${PAWSEY_SPACK_CONFIG_REPO}/scripts/create_dependencies_metamodule.sh" ${pkg} "${ENVS_DIR}/${pkg}" "${custom_full_modules_dir_zen3_gcc}"
+    [ -d "${ENVS_DIR}/${pkg}" ] || continue
+    "${PAWSEY_SPACK_CONFIG_REPO}/scripts/create_dependencies_metamodule.sh" ${pkg} "${ENVS_DIR}/${pkg}" "${custom_full_modules_dir_gcc}"
     # hiding wrf/roms dependency modules for now
-    mv "${custom_full_modules_dir_zen3_gcc}/${pkg}-dependency-set.lua" "${custom_full_modules_dir_zen3_gcc}/.${pkg}-dependency-set.lua"
+    mv "${custom_full_modules_dir_gcc}/${pkg}-dependency-set.lua" "${custom_full_modules_dir_gcc}/.${pkg}-dependency-set.lua"
 done
