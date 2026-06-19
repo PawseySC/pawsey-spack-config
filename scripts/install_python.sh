0f2b805718403f88125b893f50ce1cc02dfee09b
Edric Matwiejew <edric.matwiejew@CSIRO.au>
2026-06-19 15:13:42 +0800

    export SPACK_PYTHON

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -5,8 +5,8 @@ set_spack_config_repo
 set_compilation_sets_for_arch
 
 # for first run, use cray-python, because there is no Spack python yet
-module load cray-python
-SPACK_PYTHON="$CRAY_PYTHON_PREFIX/bin/python3"
+module load cray-python/3.11.7
+export SPACK_PYTHON="$CRAY_PYTHON_PREFIX/bin/python3"
 
 # initialise spack 
 . "${INSTALL_PREFIX}/spack/share/spack/setup-env.sh"
