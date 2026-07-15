be2fd3250fed4c99eb16907c0d3dd0a81bd3f9a7
Edric-Matwiejew <edric.matwiejew@research.uwa.edu.au.com>
2026-07-16 07:31:05 +0800

    Remove | join('.')

--- systems/setonix-q/templates/modules/modulefile.lua
+++ systems/setonix-q/templates/modules/modulefile.lua
@@ -70,7 +70,8 @@ setenv("LMOD_{{ name|upper() }}_VERSION", "{{ version_part }}")
 {% set module_name = module |replace("astro-applications/", "") |replace("bio-applications/", "") |replace("py-keras-applications/", "pawsey-temporary-string/") |replace("applications/", "") |replace("pawsey-temporary-string/", "py-keras-applications/") |replace("libraries/", "") |replace("programming-languages/", "") |replace("utilities/", "") |replace("visualisation/", "") |replace("python-packages/", "") |replace("benchmarking/", "") |replace("developer-tools/", "") |replace("dependencies/", "") |replace("project-apps/", "") |replace("user-apps/", "") %}
 {# CUDA is provided by the site module cuda/<major.minor>, not by a Spack wrapper module. #}
 {% if module_name.startswith("cuda/") or module_name.startswith(".cuda/") %}
-{% set cuda_version = module_name.split('/')[1].split('-')[0].split('.')[:2] | join('.') %}
+{% set cuda_version_parts = module_name.split('/')[1].split('-')[0].split('.') %}
+{% set cuda_version = cuda_version_parts[0] ~ "." ~ cuda_version_parts[1] %}
 {% set module_name = "cuda/" ~ cuda_version %}
 {% endif %}
 {% if verbose %}
